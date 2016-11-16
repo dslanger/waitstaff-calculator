1b0664a54fa9709dc3a02c0ccb9ab86beb91d0af
dslanger <dslanger@gmail.com>
2016-11-16 00:57:51 -0700

    initial logic and layout

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 angular.module('waitstaffApp', [])
   .controller('mainController', function() {
     var vm = this;
-    
+
+    // initial values
+
+    vm.subtotal = 0;
+    vm.tip = 0;
+    vm.total = 0;
+    vm.tipTotal = 0;
+    vm.mealCount = 0;
+    vm.avgTipPerMeal = 0;
+
+    vm.getMealTotal = function() {
+      vm.subtotal = vm.mealPrice * (1 + vm.taxRate / 100);
+      vm.tip = vm.mealPrice * (vm.tipPercentage / 100);
+      vm.total = vm.subtotal + vm.tip;
+    };
+
+    vm.getEarningsInfo = function() {
+      vm.tipTotal += vm.tip;
+      vm.mealCount += 1;
+      vm.avgTipPerMeal = vm.tipTotal / vm.mealCount;
+    };
+
+
   });
