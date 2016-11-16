912d62cb17a17e3c170156a5136498020b239518
dslanger <dslanger@gmail.com>
2016-11-16 01:11:06 -0700

    reset + cancel logic

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-angular.module('waitstaffApp', [])
+angular.module('waitstaffApp', ['ngMessages'])
   .controller('mainController', function() {
     var vm = this;
 
     // initial values
-
     vm.subtotal = 0;
     vm.tip = 0;
     vm.total = 0;
@@ -23,5 +22,20 @@ angular.module('waitstaffApp', [])
       vm.avgTipPerMeal = vm.tipTotal / vm.mealCount;
     };
 
+    vm.cancelTipForm = function() {
+      vm.mealPrice = '';
+      vm.taxRate = '';
+      vm.tipPercentage = '';
+      tipForm.$setPristine();
+    };
+
+    vm.reset = function() {
+      vm.subtotal = 0;
+      vm.tip = 0;
+      vm.total = 0;
+      vm.tipTotal = 0;
+      vm.mealCount = 0;
+      vm.avgTipPerMeal = 0;
+    };
 
   });
